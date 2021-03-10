2dc4cbd150d1ecb30d458fa2ea2e4d9c8a322e94
tfvincent <mostert.vincent@ohotmail.com>
2021-03-10 14:50:17 +0100

    Finished NestedLoopJoin Added prints

--- database_vector.cpp
+++ database_vector.cpp
@@ -12,21 +12,46 @@ vector<vector<int>> grades1;
 vector<vector<int>> grades2;
 vector<vector<int>> grades3;
 
-vector<vector<int>> nestedLoopJoin(vector<vector<int>> table1, vector<vector<int>> table2, int key) {
-  vector<vector<int>> result;
-  for(int i=0; i < table1.size(); i++){
-    for(int j=0; i < table2.size(); j++){
-      if (table1[i][key] == table2[j][key])
-        vector<int> match;
-        copy(table1[i].begin(), table1[i].end(), back_inserter(match));
-        result.push_back(match.insert(match.end(), table2[j].begin(), table2[j].end()));
-    }
+
+
+
+void printVector(vector<int> const &input){
+
+  printf("|");
+  for (int i = 0; i < input.size(); i++) {
+      cout << input[i] << '|';
   }
-  //Todo: delete matching column from the result (helper function?)
-  return result;
+  printf("\n");
 }
 
-vector<vector<int> sortMergeJoin(vector<vector<int>> table1, vector<vector<int>> table2, int key) {
+void printTable(vector<vector<int>> const &input){
+  for (int i = 0; i < input.size(); i++){
+    printVector(input[i]);
+  }
+  printf("\n");
+}
+
+void nestedLoopJoin(vector<vector<int>> table1, vector<vector<int>> table2, int key) {
+  printf("NESTEDLOOPJOIN: \n");
+  printTable(table1);
+  printTable(table2);
+  printf("RESULT: \n");
+  vector<vector<int>> result;
+  for(int i=0; i < table1.size(); i++){
+    for(int j=0; j < table2.size(); j++){
+      if (table1[i][key] == table2[j][key]){
+        vector<int> match;
+        copy(table1[i].begin(), table1[i].end(), back_inserter(match));
+        match.insert(match.end(), table2[j].begin(), table2[j].end());
+        match.erase(match.begin() + key, match.begin() + (key +1));
+        result.push_back(match);
+      }
+    }
+  }
+  printTable(result);
+}
+/*
+vector<vector<int>> sortMergeJoin(vector<vector<int>> table1, vector<vector<int>> table2, int key) {
   vector<vector<int>> copyTable1;
   vector<vector<int>> copyTable2;
   copyTable1 = Sort(table1.begin(), table1.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
@@ -36,22 +61,23 @@ vector<vector<int> sortMergeJoin(vector<vector<int>> table1, vector<vector<int>>
   return a[key] < b[key];
   });
   //Todo: merge part of the sortMergeJoin
-}
+}*/
 
 int main(){
-  printf("Work is Worship");
   grades1 = {
             {1, 2, 3},
             {4, 5, 6},
-            {7, 8, 9}}
+            {7, 8, 9}};
   grades2 = {
             {9, 8, 7},
             {6, 5, 4},
-            {3, 2, 1}}
+            {3, 2, 1}};
   grades3 = {
             {1, 1, 1},
             {8, 8, 8},
-            {4, 4, 4}}  
-  printf(nestedLoopJoin(grades1, grades2, 2));
-  printf(nestedloopJoin(grades1, grades3, 1))
+            {4, 4, 4}};
+
+  nestedLoopJoin(grades1, grades2, 1);
+  nestedLoopJoin(grades1, grades3, 0);
+  return 1;
 }
