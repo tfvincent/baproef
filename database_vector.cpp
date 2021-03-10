5332a9a5270f9e9c750b09bd40057e7c54de37bb
tfvincent <mostert.vincent@ohotmail.com>
2021-03-10 16:38:37 +0100

    Finished SortMergeJoin Added prints

--- database_vector.cpp
+++ database_vector.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -50,34 +51,89 @@ void nestedLoopJoin(vector<vector<int>> table1, vector<vector<int>> table2, int 
   }
   printTable(result);
 }
-/*
-vector<vector<int>> sortMergeJoin(vector<vector<int>> table1, vector<vector<int>> table2, int key) {
+
+
+
+vector<vector<int>> sortMergeJoin(vector<vector<int>>& table1, vector<vector<int>>& table2, int key) {
+  printf("SORTMERGEJOIN: \n");
   vector<vector<int>> copyTable1;
   vector<vector<int>> copyTable2;
-  copyTable1 = Sort(table1.begin(), table1.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-  return a[key] < b[key];
+  vector<vector<int>> result;
+  copy(table1.begin(), table1.end(), back_inserter(copyTable1));
+  copy(table2.begin(), table2.end(), back_inserter(copyTable2));
+
+  printf("BEFORE SORTING: \n");
+  printTable(copyTable1);
+  printTable(copyTable2);
+
+  sort(copyTable1.begin(), copyTable1.end(), [](const vector<int>& a, const vector<int>& b) {
+  return a[0] < b[0];
   });
-  copyTable2 = Sort(table2.begin(), table2.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-  return a[key] < b[key];
+  sort(copyTable2.begin(), copyTable2.end(), [](const vector<int>& a, const vector<int>& b) {
+  return a[0] < b[0];
   });
-  //Todo: merge part of the sortMergeJoin
-}*/
+
+  printf("AFTER SORTING: \n");
+  printTable(copyTable1);
+  printTable(copyTable2);
+
+  int mark = 999;
+  int table1idx = 0;
+  int table2idx = 0;
+
+  printf("RESULT: \n");
+  do {
+
+    if (mark == 999) {
+      while(copyTable1[table1idx][key] < copyTable2[table2idx][key]) {
+        table1idx += 1;
+      }
+      while(copyTable1[table1idx][key] > copyTable2[table2idx][key]) {
+        table2idx += 1;
+      }
+      mark = table2idx;
+    }
+    if (copyTable1[table1idx][key] == copyTable2[table2idx][key]) {
+      vector<int> match;
+      copy(copyTable1[table1idx].begin(), copyTable1[table1idx].end(), back_inserter(match));
+      match.insert(match.end(), copyTable2[table2idx].begin(), copyTable2[table2idx].end());
+      match.erase(match.begin() + key, match.begin() + (key +1));
+      printVector(match);
+      result.push_back(match);
+      table2idx += 1;
+    } else {
+      table2idx = mark;
+      table1idx += 1;
+      mark = 999;
+    }
+  }
+  while (table1idx < copyTable1.size());
+
+  printTable(result);
+
+}
 
 int main(){
   grades1 = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}};
+            {22, 2},
+            {28, 5},
+            {31, 8},
+            {31, 82},
+            {44, 4},
+            {58, 9}};
   grades2 = {
-            {9, 8, 7},
-            {6, 5, 4},
-            {3, 2, 1}};
+            {28, 103},
+            {28, 104},
+            {31, 101},
+            {31, 102},
+            {42, 142},
+            {58, 107}};
   grades3 = {
             {1, 1, 1},
             {8, 8, 8},
             {4, 4, 4}};
 
-  nestedLoopJoin(grades1, grades2, 1);
-  nestedLoopJoin(grades1, grades3, 0);
+  nestedLoopJoin(grades1, grades2, 0);
+  sortMergeJoin(grades1, grades2, 0);
   return 1;
 }
